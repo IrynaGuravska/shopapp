3fc19226c4fc71b7103c593d93dd706cdcf74409
Iryna Guravska <irinaguru.ua@gmail.com>
2023-02-28 22:21:13 +0200

    Add Product

--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -1,37 +1,68 @@
 import styles from './Product.module.scss';
 import clsx from 'clsx';
 import Button from '../Button/Button';
+import { useState } from 'react';
 
 const Product = props => {
+
+  const [currentColor, setCurrentColor] = useState(props.colors[0]);
+  const [currentSize, setCurrentSize] = useState('S');
+  const [currentSizePrice, setCurrentSizePrice] = useState(props.sizes[0].additionalPrice);
+
+  const prepareColorClassName = color => {
+    return styles['color' + color[0].toUpperCase() + color.substr(1).toLowerCase()];
+  }
+
+  const getPrice = () => {
+    return props.basePrice + currentSizePrice;
+  }
+
+  const shirtInBasket = (event) => {
+    event.preventDefault();
+    return (
+      console.log('Summary'),
+      console.log('========='),
+      console.log('Name: ', props.title),
+      console.log('Price: ', getPrice()),
+      console.log('Size: ', currentSize),
+      console.log('Color: ', currentColor)
+    )
+  }
+
   return (
     <article className={styles.product}>
       <div className={styles.imageContainer}>
         <img 
           className={styles.image}
-          alt="Kodilla shirt"
-          src={`${process.env.PUBLIC_URL}/images/products/shirt-kodilla--black.jpg`} />
+          alt={`${props.name} shirt`}
+          src={`${process.env.PUBLIC_URL}/images/products/shirt-${props.name}--${currentColor}.jpg`} />
       </div>
       <div>
         <header>
-          <h2 className={styles.name}>Kodilla shirt</h2>
-          <span className={styles.price}>Price: 20$</span>
+          <h2 className={styles.name}>{props.title}</h2>
+          <span className={styles.price}>Price: {getPrice()}$</span>
         </header>
-        <form>
+        <form onSubmit={shirtInBasket}>
           <div className={styles.sizes}>
             <h3 className={styles.optionLabel}>Sizes</h3>
             <ul className={styles.choices}>
-              <li><button type="button" className={styles.active}>S</button></li>
-              <li><button type="button">M</button></li>
-              <li><button type="button">L</button></li>
-              <li><button type="button">XL</button></li>
+              {props.sizes.map(size => (
+                <li key={size}>
+                  <button type="button" onClick={() => {setCurrentSize(size.name); setCurrentSizePrice(size.additionalPrice)}} className={size.name === currentSize && styles.active}>
+                    {size.name}
+                  </button>
+                </li>
+              ))}
             </ul>
           </div>
           <div className={styles.colors}>
             <h3 className={styles.optionLabel}>Colors</h3>
             <ul className={styles.choices}>
-              <li><button type="button" className={clsx(styles.colorBlack, styles.active)} /></li>
-              <li><button type="button" className={clsx(styles.colorRed)} /></li>
-              <li><button type="button" className={clsx(styles.colorWhite)} /></li>
+              {props.colors.map(item => (
+                <li key={item}>
+                  <button type="button" onClick={() => setCurrentColor(item)} className={clsx(prepareColorClassName(item), item === currentColor && styles.active)} />
+                </li>
+              ))}
             </ul>
           </div>
           <Button className={styles.button}>
@@ -42,5 +73,4 @@ const Product = props => {
     </article>
   )
 };
-
 export default Product;
