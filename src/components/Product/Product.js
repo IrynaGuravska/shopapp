253610e88233e63fc122add7088098c235861b2e
Iryna Guravska <irinaguru.ua@gmail.com>
2023-03-01 01:03:45 +0200

    Add folder to components

--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -2,6 +2,7 @@ import styles from './Product.module.scss';
 import clsx from 'clsx';
 import Button from '../Button/Button';
 import { useState } from 'react';
+import ProductImage from './ProductImage/ProductImage';
 
 const Product = props => {
 
@@ -31,12 +32,7 @@ const Product = props => {
 
   return (
     <article className={styles.product}>
-      <div className={styles.imageContainer}>
-        <img 
-          className={styles.image}
-          alt={`${props.name} shirt`}
-          src={`${process.env.PUBLIC_URL}/images/products/shirt-${props.name}--${currentColor}.jpg`} />
-      </div>
+      <ProductImage name={props.name} color={currentColor} />
       <div>
         <header>
           <h2 className={styles.name}>{props.title}</h2>
